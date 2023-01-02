e0a6613a444144667db4a6db51bc4e89062de70e
peter robert <pwambui93@gmail.com>
2023-01-02 20:57:48 +0300

    Revert "Feature/category product"

--- app/models/product.rb
+++
@@ -1,8 +0,0 @@
-class Product < ApplicationRecord
-  belongs_to :category
-
-  validates :title, presence: true, length: {minimum: 3}
-  validates :price, presence: true
-  validates :description, presence: true
-  validates :image, presence: true
-end
--- spec/factories/product.rb
+++
@@ -1,9 +0,0 @@
-FactoryBot.define do
-    factory :product do
-      title {"Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops"}
-      price  {109.95}
-      category { create(:category) }
-      image  {"https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg"}
-      description {"Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday"}
-    end
-end
